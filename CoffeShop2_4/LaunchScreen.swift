e5fb90bda14cdaae36b311b50c39449cd60936a2
Charles Choi <cchoi1065@stu.d214.org>
2018-12-10 14:09:50 -0600

    launch screen done

--- CoffeShop2_4/LaunchScreen.swift
+++ CoffeShop2_4/LaunchScreen.swift
@@ -7,16 +7,23 @@
 //
 
 import UIKit
-
-class LaunchScreen: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        
-    }
-    
-
-   
-
-}
+//
+//class LaunchScreen: UIViewController {
+//    
+//    
+//    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        var seconds = 5
+//        var timerRunning = false
+//        func runTimer() {
+//            time = Timer.scheduledTimer(timeInterval: 1, target: self, selector: (#selector(LaunchScreen.load)), userInfo: nil, repeats: false)
+//                    }
+//        func update() {
+//            seconds -= 1
+//        }
+//        runTimer()
+//        
+//    }
+//    
+//}
