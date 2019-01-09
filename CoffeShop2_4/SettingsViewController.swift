20ac8f3f2de2015309fa8f6dd0d19c2d7ff3a43a
Illya.Kochylo <ikochylo1431@stu.d214.org>
2019-01-09 13:50:39 -0600

    slide view added

---
+++ CoffeShop2_4/SettingsViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SettingsViewController.swift
+//  CoffeShop2_4
+//
+//  Created by Illya Kochylo on 12/20/18.
+//  Copyright © 2018 Illya Kochylo. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    @IBOutlet weak var two: UIBarButtonItem!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+   
+}
---
+++ CoffeShop2_4/TransactionsViewController.swift
@@ -0,0 +1,22 @@
+//
+//  TransactionsViewController.swift
+//  CoffeShop2_4
+//
+//  Created by Illya Kochylo on 12/20/18.
+//  Copyright © 2018 Illya Kochylo. All rights reserved.
+//
+
+import UIKit
+
+class TransactionsViewController: UIViewController {
+
+    @IBOutlet weak var four: UINavigationItem!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+   
+}
