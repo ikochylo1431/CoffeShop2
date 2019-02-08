95202f8b0ee57e902b1bcc94e80d64d5389134a0
Illya.Kochylo <ikochylo1431@stu.d214.org>
2019-02-08 14:11:46 -0600

    added calculator buttons

--- CoffeShop2_4/CalculatorViewController.swift
+++ CoffeShop2_4/CalculatorViewController.swift
@@ -13,6 +13,42 @@ class CalculatorViewController: UIViewController {
   
     @IBOutlet weak var item: UIBarButtonItem!
     
+    @IBAction func nilCalc(_ sender: UIButton) {
+    }
+    @IBAction func oneCalc(_ sender: UIButton) {
+    }
+    @IBAction func twoCalc(_ sender: UIButton) {
+    }
+    @IBAction func threeCalc(_ sender: UIButton) {
+    }
+    @IBAction func fourCalc(_ sender: UIButton) {
+    }
+    @IBAction func fiveCalc(_ sender: UIButton) {
+    }
+    @IBAction func sixCalc(_ sender: UIButton) {
+    }
+    @IBAction func sevenCalc(_ sender: UIButton) {
+    }
+    @IBAction func eightCalc(_ sender: UIButton) {
+    }
+    @IBAction func nineCalc(_ sender: UIButton) {
+    }
+    @IBAction func clearCalc(_ sender: UIButton) {
+    }
+    @IBAction func equalCalc(_ sender: UIButton) {
+    }
+    @IBAction func divideCalc(_ sender: UIButton) {
+    }
+    @IBAction func multiplyCalc(_ sender: UIButton) {
+    }
+    @IBAction func minusCalc(_ sender: UIButton) {
+    }
+    @IBAction func plusCalc(_ sender: UIButton) {
+    }
+    @IBOutlet weak var calculatorNumbers: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         item.target = revealViewController()
