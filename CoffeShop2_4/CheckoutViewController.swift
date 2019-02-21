d0af9f474fc0030d1253e5f600d0eb3f6ee46dbb
Charles Choi <cchoi1065@stu.d214.org>
2019-02-21 14:11:21 -0600

    clear button

--- CoffeShop2_4/CheckoutViewController.swift
+++ CoffeShop2_4/CheckoutViewController.swift
@@ -5,29 +5,39 @@
 //  Created by Illya Kochylo on 12/4/18.
 //  Copyright © 2018 Illya Kochylo. All rights reserved.
 //
-///////////
+//
 
 
 import UIKit
 
 class CheckoutViewController: UIViewController {
 
-   
+    @IBOutlet weak var checkoutCollectionView: UICollectionView!
     
+var collectionData = ["1", "1", "1", "1", "1", "1", "1", "1", "1"]
 
-    @IBOutlet weak var item: UIBarButtonItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    item.target = revealViewController()!
-        item.action = Selector("revealToggle:")
-            
+        let width = (view.frame.size.width - 20) / 3
+        let layout = checkoutCollectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        layout.itemSize = CGSize(width: width, height: width)
+        
     }
-    var String = "Hello moto"
-
-
-
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
 }
+
+//extension CheckoutViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//        return collectionData.count
+//    }
+//
+//    func collection(_collectio)
+//
+
+
 
 
 
