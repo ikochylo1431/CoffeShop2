30cfb353bbcbeaf15e4c0224f7e35c036e3ae37c
Charles Choi <cchoi1065@stu.d214.org>
2019-03-18 14:10:33 -0500

    Yeeeeeet

--- CoffeShop2_4/ItemsViewController.swift
+++ CoffeShop2_4/ItemsViewController.swift
@@ -7,22 +7,111 @@
 //
 
 import UIKit
+import Firebase
 
-class ItemsViewController: UIViewController {
+class ItemsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
-    
+    var itemArray = [Item]()
     
     @IBOutlet weak var item: UIBarButtonItem!
+    @IBOutlet weak var itemTableView: UITableView!
     
+
+    var db: DatabaseReference!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         item.target = revealViewController()
         item.action = Selector("revealToggle:")
+
+    itemTableView.delegate = self
+    itemTableView.dataSource = self
+    db = Database.database().reference().child("item")
     }
     
-
-  
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+    itemTableView.reloadData()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return itemArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell:UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "myCell")!
+    cell.textLabel?.text = itemArray[indexPath.row].name
+    return cell
+    
+    }
+    
+    
+    
+    
+    @IBAction func onAddButtonPressed(_ sender: UIBarButtonItem) {
+    let alert = UIAlertController(title: "Add New Item", message: nil, preferredStyle: UIAlertController.Style.alert)
+    
+    alert.addTextField { (textField) in
+    textField.placeholder = "Add Item Name Here"
+    }
+    
+    alert.addTextField { (textField) in
+    textField.placeholder = "Add Description of Item Here"
+    }
+    
+    alert.addTextField { (textField) in
+    textField.placeholder = "Add Cost Here"
+    }
+    
+    
+    
+    let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertAction.Style.cancel, handler: nil)
+    alert.addAction(cancelAction)
+    let addAction = UIAlertAction(title: "Add", style: UIAlertAction.Style.default) { (action) in
+    let nameOfItem = alert.textFields?[0]
+    let itemDescription = alert.textFields?[1]
+    let costOfItem = alert.textFields?[2]
+        let newItem = Item(name: (nameOfItem?.text)!, description: (itemDescription?.text)!, cost: Int((costOfItem?.text)!)!)
+    self.db.child(newItem.name).child("Item Name").setValue(newItem.name)
+    self.db.child(newItem.name).child("Item Description").setValue(newItem.description)
+        self.db.child(newItem.name).child("Cost of Item").setValue(newItem.cost)
+    self.itemArray.append(newItem)
+    self.itemTableView.reloadData()
+    }
+    
+    alert.addAction(addAction)
+    present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func onEditButtonPressed(_ sender: UIBarButtonItem) {
+    if sender.tag == 0 {
+    itemTableView.isEditing = true
+    sender.tag = 1
+    }
+    else {
+    itemTableView.isEditing = false
+    sender.tag = 0
+    }
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+    if editingStyle == UITableViewCell.EditingStyle.delete {
+    itemArray.remove(at: indexPath.row)
+    itemTableView.reloadData()
+    
+    }
+    }
+    
+    
+    
+    func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+    return true
+    }
+    
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+    let ITEM = itemArray[sourceIndexPath.row]
+    itemArray.remove(at: sourceIndexPath.row)
+    itemArray.insert(ITEM, at: destinationIndexPath.row)
+    }
 }
